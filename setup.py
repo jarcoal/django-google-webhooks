e919db459a821be5ee525e1036cfe0439caaf25c
Jared Morse <jarcoal@gmail.com>
2015-12-26 10:26:33 -0800

    initial distutils setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Django Google Webhooks',
+    version='0.0.1',
+    description='Django views for working with Google API webhooks',
+    author='Jared Morse',
+    author_email='jarcoal@gmail.com',
+    url='https://github.com/jarcoal/django-google-webhooks',
+    packages=['google_webhooks'],
+)
